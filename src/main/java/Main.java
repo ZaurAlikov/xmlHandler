76c3571115698b0098d8b838f7448a93fcec4cc1
Аликов Заурбек Олегович <Alikov.Z.O@sberbank.ru>
2019-07-26 15:39:32 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,96 @@
+import model.ATuningPrice;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        readFromExcel("/home/user/Загрузки/Прайс туле 3 с 23.07.2019.xlsx");
+    }
+
+    public static void readFromExcel(String filePath) throws IOException {
+        XSSFWorkbook myExcelBook = new XSSFWorkbook(new FileInputStream(new File(filePath)));
+        XSSFSheet myExcelSheet = myExcelBook.getSheet("TDSheet");
+        List<XSSFRow> rows = new ArrayList<>();
+        List<ATuningPrice> aTuningPrices = new ArrayList<>();
+        String category = "";
+        String subCategory = "";
+        for (int i = 0; i < myExcelSheet.getLastRowNum(); i++) {
+            rows.add(myExcelSheet.getRow(i));
+        }
+        if (rows.size() > 0) {
+
+            for (int i = 1026; i < rows.size(); i++) {
+                if (getColor(rows, i, 0).equals("FBF9EC") && getColor(rows, i+1, 0).equals("FBF9EC")) {
+                    category = checkCellGetString(rows.get(i).getCell(0));
+                    subCategory = checkCellGetString(rows.get(i+1).getCell(0));
+                } else if (getColor(rows, i, 0).equals("FBF9EC") && getColor(rows, i-1, 0).equals("") && getColor(rows, i+1, 0).equals("")) {
+                    category = checkCellGetString(rows.get(i).getCell(0));
+                    subCategory = "";
+                }
+                if (getColor(rows, i, 0).equals("")) {
+                    ATuningPrice aTuningPrice = new ATuningPrice();
+                    aTuningPrice.setProductCategory(category);
+                    aTuningPrice.setProductSubCategory(subCategory);
+                    aTuningPrice.setProductName(checkCellGetString(rows.get(i).getCell(0)));
+                    aTuningPrice.setSKU(checkCellGetString(rows.get(i).getCell(12)));
+                    aTuningPrice.setRetailPrice(checkCellGetBigDec(rows.get(i).getCell(14)));
+                    aTuningPrice.setTradePrice(checkCellGetBigDec(rows.get(i).getCell(15)));
+                    aTuningPrices.add(aTuningPrice);
+                }
+            }
+        }
+        System.out.println(aTuningPrices);
+
+        XSSFRow row = myExcelSheet.getRow(0);
+
+
+        if (row.getCell(0).getCellType() == CellType.STRING) {
+            String name = row.getCell(0).getStringCellValue();
+            System.out.println("name : " + name);
+        }
+
+        if (row.getCell(1).getCellType() == CellType.NUMERIC) {
+            Date birthdate = row.getCell(1).getDateCellValue();
+            System.out.println("birthdate :" + birthdate);
+        }
+
+        myExcelBook.close();
+
+    }
+
+    private static String checkCellGetString(XSSFCell cell) {
+        if (cell.getCellType() == CellType.STRING) {
+            return cell.getStringCellValue();
+        }
+        if (cell.getCellType() == CellType.NUMERIC) {
+            return String.valueOf(new Double(cell.getNumericCellValue()).intValue());
+        }
+        return "";
+    }
+
+    private static BigDecimal checkCellGetBigDec(XSSFCell cell) {
+        if (cell.getCellType() == CellType.NUMERIC) {
+            return new BigDecimal(cell.getNumericCellValue());
+        }
+        return BigDecimal.ZERO;
+    }
+
+    private static String getColor(List<XSSFRow> rows, int rowNum, int cellNum) {
+        String color = "";
+        XSSFColor fillForegroundXSSFColor = rows.get(rowNum).getCell(cellNum).getCellStyle().getFillForegroundXSSFColor();
+        if (fillForegroundXSSFColor != null) {
+            color = fillForegroundXSSFColor.getARGBHex().substring(2);
+        }
+        return color;
+    }
+
+}
---
+++ src/main/java/model/ATuningPrice.java
@@ -0,0 +1,60 @@
+package model;
+
+import java.math.BigDecimal;
+
+public class ATuningPrice {
+    private String productCategory;
+    private String productSubCategory;
+    private String productName;
+    private String SKU;
+    private BigDecimal tradePrice;
+    private BigDecimal retailPrice;
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public String getProductSubCategory() {
+        return productSubCategory;
+    }
+
+    public void setProductSubCategory(String productSubCategory) {
+        this.productSubCategory = productSubCategory;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getSKU() {
+        return SKU;
+    }
+
+    public void setSKU(String SKU) {
+        this.SKU = SKU;
+    }
+
+    public BigDecimal getTradePrice() {
+        return tradePrice;
+    }
+
+    public void setTradePrice(BigDecimal tradePrice) {
+        this.tradePrice = tradePrice;
+    }
+
+    public BigDecimal getRetailPrice() {
+        return retailPrice;
+    }
+
+    public void setRetailPrice(BigDecimal retailPrice) {
+        this.retailPrice = retailPrice;
+    }
+}

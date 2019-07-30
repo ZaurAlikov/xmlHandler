339d12fe10470847167c9102568fe6ea6729a837
Аликов Заурбек Олегович <Alikov.Z.O@sberbank.ru>
2019-07-30 18:03:16 +0300

    commit_gmail

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,42 @@
+import javax.mail.MessagingException;
 import java.io.IOException;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.Label;
+import com.google.api.services.gmail.model.ListLabelsResponse;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
+import static service.GmailQuickstart.getCredentials;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String APPLICATION_NAME = "Gmail API Java Quickstart";
+
+    public static void main(String[] args) throws IOException, MessagingException, GeneralSecurityException {
+        // Build a new authorized API client service.
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+
+        // Print the labels in the user's account.
+        String user = "me";
+        ListLabelsResponse listResponse = service.users().labels().list(user).execute();
+        List<Label> labels = listResponse.getLabels();
+        if (labels.isEmpty()) {
+            System.out.println("No labels found.");
+        } else {
+            System.out.println("Labels:");
+            for (Label label : labels) {
+                System.out.printf("- %s\n", label.getName());
+            }
+        }
+
         MainReader reader = new MainReader();
         reader.read();
     }
